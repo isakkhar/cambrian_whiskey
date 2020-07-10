9c2da1d65bdf76fe34d7e572e15f36258acd21ce
aledgriffiths79 <aledgriffiths08@hotmail.co.uk>
2020-07-10 17:40:01 +0100

    added a registration option by adding a registration button for user in the login page

--- checkout/views.py
+++ checkout/views.py
@@ -48,7 +48,7 @@ def checkout(request):
       try:
         customer = stripe.Charge.create(
           amount = int(total * 100),
-          currency = 'UK',
+          currency = 'gbp',
           # description: if we go to our stripe dashboard we would be able to see who the payment came from
           description = request.user.email,
           card = payment_form.cleaned_data['stripe_id'],
